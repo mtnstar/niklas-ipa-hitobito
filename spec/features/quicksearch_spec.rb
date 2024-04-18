5a759c41cbcacb1fd94899299615671d29bab900
Niklas Jäggi <jaeggi@puzzle.ch>
2024-04-18 14:07:48 +0200

    Adjust Feature spec to search address #16

--- spec/features/quicksearch_spec.rb
+++ spec/features/quicksearch_spec.rb
@@ -37,18 +37,18 @@ describe 'Quicksearch' do
         dropdown = find('ul[role="listbox"]')
         expect(dropdown).to have_content('Top Leader, Supertown')
         expect(dropdown).to have_content('Top → TopGroup')
-        expect(dropdown).to have_content('Top')
 
         fill_in 'quicksearch', with: 'Top Leader'
         expect(dropdown).to have_content('Top Leader, Supertown')
+        expect(dropdown).not_to have_content('Top → TopGroup')
 
         fill_in 'quicksearch', with: 'TopGroup'
+        expect(dropdown).not_to have_content('Top Leader, Supertown')
         expect(dropdown).to have_content('Top → TopGroup')
 
-        fill_in 'quicksearch', with: 'TopGroup'
+        fill_in 'quicksearch', with: 'Supertown'
         expect(dropdown).to have_content('Top Leader, Supertown')
-
-
+        expect(dropdown).not_to have_content('Top → TopGroup')
       end
     end
   end
